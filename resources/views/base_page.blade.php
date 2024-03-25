2e21b289f0f89e2799fd947a9edcc712cb584edb
vsh2607 <evolutionvalen@gmail.com>
2024-03-25 23:33:21 +0700

    Change secure_asset to asset

--- resources/views/base_page.blade.php
+++ resources/views/base_page.blade.php
@@ -45,7 +45,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
-    <script src="{{ secure_asset('scripts/index.js') }}"></script>
+    <script src="{{ asset('scripts/index.js') }}"></script>
 </body>
 
 </html>
